58193ffdfaba2e54b27dba40ac64f58901b57a28
Aluno <iago.olvr@gmail.com>
2016-11-10 21:48:15 -0200

    Create a new Development start class using the development profile opstions.

--- src/main/java/domain/Development.java
+++ src/main/java/domain/Development.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Development {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        System.setProperty("spring.profiles.active", "development");
+        SpringApplication.run(Development.class, args);
     }
 }
